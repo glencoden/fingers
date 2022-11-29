42c33f35d9001468061319e6d922124ff81a1503
glen coden <simon.der.meyer@gmail.com>
2022-11-29 11:54:20 +0100

    add weight to characters

--- script.js
+++ script.js
@@ -1,3 +1,111 @@
+const tasks = [
+    { value: '`', weight: 2 },
+    { value: '1', weight: 2 },
+    { value: '2', weight: 2 },
+    { value: '3', weight: 2 },
+    { value: '4', weight: 2 },
+    { value: '5', weight: 2 },
+    { value: '6', weight: 2 },
+    { value: '7', weight: 2 },
+    { value: '8', weight: 2 },
+    { value: '9', weight: 2 },
+    { value: '0', weight: 2 },
+    { value: '-', weight: 2 },
+    { value: '=', weight: 2 },
+    { value: 'Backspace', weight: 1 },
+    { value: 'Tab', weight: 1 },
+    { value: 'q', weight: 1 },
+    { value: 'w', weight: 1 },
+    { value: 'e', weight: 1 },
+    { value: 'r', weight: 1 },
+    { value: 't', weight: 1 },
+    { value: 'y', weight: 1 },
+    { value: 'u', weight: 1 },
+    { value: 'i', weight: 1 },
+    { value: 'o', weight: 1 },
+    { value: 'p', weight: 1 },
+    { value: '[', weight: 2 },
+    { value: ']', weight: 2 },
+    { value: '\\', weight: 2 },
+    { value: 'a', weight: 1 },
+    { value: 's', weight: 1 },
+    { value: 'd', weight: 1 },
+    { value: 'f', weight: 1 },
+    { value: 'g', weight: 1 },
+    { value: 'h', weight: 1 },
+    { value: 'j', weight: 1 },
+    { value: 'k', weight: 1 },
+    { value: 'l', weight: 1 },
+    { value: ';', weight: 1 },
+    { value: `'`, weight: 1 },
+    { value: 'Enter', weight: 1 },
+    { value: 'z', weight: 1 },
+    { value: 'x', weight: 1 },
+    { value: 'c', weight: 1 },
+    { value: 'v', weight: 1 },
+    { value: 'b', weight: 1 },
+    { value: 'n', weight: 1 },
+    { value: 'm', weight: 1 },
+    { value: ',', weight: 1 },
+    { value: '.', weight: 1 },
+    { value: '/', weight: 2 },
+    { value: 'ArrowUp', weight: 1 },
+    { value: 'Control', weight: 2 },
+    { value: 'Alt', weight: 2 },
+    { value: 'Meta', weight: 2 },
+    { value: ' ', weight: 0 },
+    { value: 'ArrowLeft', weight: 1 },
+    { value: 'ArrowDown', weight: 1 },
+    { value: 'ArrowRight', weight: 1 },
+    { value: '~', weight: 2 },
+    { value: '!', weight: 2 },
+    { value: '@', weight: 2 },
+    { value: '#', weight: 2 },
+    { value: '$', weight: 2 },
+    { value: '%', weight: 2 },
+    { value: '^', weight: 2 },
+    { value: '&', weight: 2 },
+    { value: '*', weight: 2 },
+    { value: '(', weight: 2 },
+    { value: ')', weight: 2 },
+    { value: '_', weight: 2 },
+    { value: '+', weight: 2 },
+    { value: 'Q', weight: 1 },
+    { value: 'W', weight: 1 },
+    { value: 'E', weight: 1 },
+    { value: 'R', weight: 1 },
+    { value: 'T', weight: 1 },
+    { value: 'Y', weight: 1 },
+    { value: 'U', weight: 1 },
+    { value: 'I', weight: 1 },
+    { value: 'O', weight: 1 },
+    { value: 'P', weight: 1 },
+    { value: '{', weight: 2 },
+    { value: '}', weight: 2 },
+    { value: '|', weight: 2 },
+    { value: 'A', weight: 1 },
+    { value: 'S', weight: 1 },
+    { value: 'D', weight: 1 },
+    { value: 'F', weight: 1 },
+    { value: 'G', weight: 1 },
+    { value: 'H', weight: 1 },
+    { value: 'J', weight: 1 },
+    { value: 'K', weight: 1 },
+    { value: 'L', weight: 1 },
+    { value: ':', weight: 1 },
+    { value: '"', weight: 1 },
+    { value: 'Z', weight: 1 },
+    { value: 'X', weight: 1 },
+    { value: 'C', weight: 1 },
+    { value: 'V', weight: 1 },
+    { value: 'B', weight: 1 },
+    { value: 'N', weight: 1 },
+    { value: 'M', weight: 1 },
+    { value: '<', weight: 2 },
+    { value: '>', weight: 2 },
+    { value: '?', weight: 1 },
+];
+
 function pollDomElements() {
     const taskElement = document.querySelector('.task');
     const makesElement = document.querySelector('.makes');
@@ -19,8 +127,6 @@ pollDomElements();
 function runScript(taskElement, makesElement, numMakesElement, numMistakesElement, taskLengthInput, averageKeyTimeElement) {
     const INITIAL_TASK_LENGTH = 1;
     const MAX_TASK_LENGTH = 12;
-
-    const tasks = [ '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', `'`, 'Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Control', 'Alt', 'Meta', ' ', 'ArrowLeft', 'ArrowDown', 'ArrowRight', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?' ];
 
     const currentTasks = [];
     let currentMakes = [];
@@ -68,8 +174,14 @@ function runScript(taskElement, makesElement, numMakesElement, numMistakesElemen
 
     function resetTasks() {
         for (let i = 0; i < taskLengthInput.value; i++) {
-            const randomTaskIndex = Math.floor(Math.random() * tasks.length);
-            const randomKey = tasks[randomTaskIndex];
+            const pickList = [];
+            tasks.forEach(task => {
+                for (let i = 0; i < task.weight; i++) {
+                    pickList.push(task.value);
+                }
+            });
+            const randomTaskIndex = Math.floor(Math.random() * pickList.length);
+            const randomKey = pickList[randomTaskIndex];
             currentTasks.push(randomKey);
         }
         updateUI();
